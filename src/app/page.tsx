a5d53f99e7aa47e7868daa51ded53c155654f72b
Mikita <ghjhjr11@gmail.com>
2025-10-03 12:40:07 +0300

    tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
+import StatusLabel, { Status } from "./components/StatusLabel/StatusLabel";
 
 export default function Home() {
   return (
     <div>
       <main>
-        <h1>Home page</h1>
+        <h1 className="bg-red-400">Home page</h1>
+        <StatusLabel status={Status.Active}>LabelLabel</StatusLabel>
+        <StatusLabel status={Status.NotActive}>LabelLabel</StatusLabel>
+        <StatusLabel status={Status.Pending}>LabelLabel</StatusLabel>
+        <StatusLabel status={Status.Suspended}>LabelLabel</StatusLabel>
       </main>
     </div>
   );
+  ``;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./CRM/src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
